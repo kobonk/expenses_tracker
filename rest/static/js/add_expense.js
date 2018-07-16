fcac7399bd93bff432fa5248cf1fcae482c96198
kobonk <kobonk@kobonk.pl>
2018-07-16 19:37:59 +0200

    Fixed stupid copy-paste bug

--- rest/static/js/add_expense.js
+++ rest/static/js/add_expense.js
@@ -21,7 +21,7 @@
 
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("demo").innerHTML = this.responseText;
+                console.log("Expense added.");
             }
         };
 
