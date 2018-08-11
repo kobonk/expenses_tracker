93446fae9d79f810814a8824931f4b63f359a976
kobonk <kobonk@kobonk.pl>
2018-08-11 18:19:05 +0200

    Refactored SqliteExpensesRetriever parameters validation

--- storage/SqliteExpensesRetriever.py
+++ storage/SqliteExpensesRetriever.py
@@ -6,24 +6,9 @@ class SqliteExpensesRetriever():
     
     def __init__(self, expenses_table_name, categories_table_name,
                  connection_provider):
-        if (not expenses_table_name or 
-            not isinstance(expenses_table_name, str)):
-            raise ValueError("InvalidArgument: expenses_table_name must be a "
-                             "non-empty string")
-
-        if (not categories_table_name or 
-            not isinstance(categories_table_name, str)):
-            raise ValueError("InvalidArgument: categories_table_name must be a "
-                             "non-empty string")
-
-        if not connection_provider:
-            raise ValueError("InvalidArgument: connection_provider must be "
-                             "provided")
-        
-        if (not hasattr(connection_provider, "get_connection") or
-            not callable(connection_provider.get_connection)):
-            raise ValueError("InvalidArgument: connection_provider must have "
-                             "get_connection method")
+        self.__validate_expenses_table_name(expenses_table_name)
+        self.__validate_categories_table_name(categories_table_name)
+        self.__validate_connection_provider(connection_provider)        
 
         self.__expenses_table_name = expenses_table_name
         self.__categories_table_name = categories_table_name
@@ -88,3 +73,25 @@ class SqliteExpensesRetriever():
 
     def __convert_table_row_to_category(self, table_row):
         return Category(table_row[0], html.unescape(table_row[1]))
+
+    def __validate_expenses_table_name(self, expenses_table_name):
+        if (not expenses_table_name or 
+            not isinstance(expenses_table_name, str)):
+            raise ValueError("InvalidArgument: expenses_table_name must be a "
+                             "non-empty string")
+
+    def __validate_categories_table_name(self, categories_table_name):
+        if (not categories_table_name or 
+            not isinstance(categories_table_name, str)):
+            raise ValueError("InvalidArgument: categories_table_name must be a "
+                             "non-empty string")
+
+    def __validate_connection_provider(self, connection_provider):
+        if not connection_provider:
+            raise ValueError("InvalidArgument: connection_provider must be "
+                             "provided")
+        
+        if (not hasattr(connection_provider, "get_connection") or
+            not callable(connection_provider.get_connection)):
+            raise ValueError("InvalidArgument: connection_provider must have "
+                             "get_connection method")
