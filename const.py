4b03deb9b8d92b44a3c7e369a923c33b76285905
kobonk <kobonk@kobonk.pl>
2019-07-09 20:38:58 +0200

    DATABASE_PATH is now OS-agnostic

--- const.py
+++ const.py
@@ -1,6 +1,7 @@
 import os
+from pathlib import Path
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-DATABASE_PATH = r"{DIR_PATH}\dbs\expenses-tracker.db".format(**locals()).replace("\\", "\\\\")
+DATABASE_PATH = str(Path("{DIR_PATH}/dbs/expenses-tracker.db".format(**locals())))
 EXPENSES_TABLE_NAME = "expenses"
 CATEGORIES_TABLE_NAME = "categories"
