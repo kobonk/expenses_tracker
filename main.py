f178fc11c54f711f28ba8812dae4601610a2ec57
kobonk <kobonk@kobonk.pl>
2019-08-13 14:39:38 +0200

    Renamed DATABASE_PATH const to SQLITE_DATABASE_PATH

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from rest.routes import app
-from const import DATABASE_PATH, DATABASE_TYPE, DATABASE_TABLES
+from const import SQLITE_DATABASE_PATH, DATABASE_TYPE, DATABASE_TABLES
 from storage.ExpensesRetrieverFactory import ExpensesRetrieverFactory
 
 if __name__ == "__main__":
-    retriever = ExpensesRetrieverFactory.create(DATABASE_TYPE, DATABASE_PATH,
+    retriever = ExpensesRetrieverFactory.create(DATABASE_TYPE, SQLITE_DATABASE_PATH,
                                                 DATABASE_TABLES)
 
     retriever.ensure_necessary_tables_exist()
