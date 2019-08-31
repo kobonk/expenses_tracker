019d53f31e17715648a29c8abf1544ea3bc6e275
kobonk <kobonk@kobonk.pl>
2019-08-31 10:47:39 +0200

    Added necessary database tables validation at application start

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from rest.routes import app
-from const import DEBUG_MODE
+from const import DATABASE_TYPE, DEBUG_MODE
+from storage.DatabaseConnectionProviderFactory import DatabaseConnectionProviderFactory
 
 if __name__ == "__main__":
+    connection_provider = DatabaseConnectionProviderFactory.create(DATABASE_TYPE)
+
+    connection_provider.ensure_necessary_tables_exist()
     app.run(debug=DEBUG_MODE)
---
+++ storage/DatabaseConnectionProviderFactory.py
@@ -0,0 +1,14 @@
+from storage.SqliteDatabaseConnectionProvider import SqliteDatabaseConnectionProvider
+from validation_utils import validate_non_empty_string
+from const import DATABASE_TABLES, DATABASE_TYPES, SQLITE_DATABASE_PATH
+
+class DatabaseConnectionProviderFactory:
+    @staticmethod
+    def create(type):
+        validate_non_empty_string(type, "type")
+
+        if type is DATABASE_TYPES["sqlite"]:
+            return SqliteDatabaseConnectionProvider(
+                SQLITE_DATABASE_PATH, DATABASE_TABLES)
+
+        return None
